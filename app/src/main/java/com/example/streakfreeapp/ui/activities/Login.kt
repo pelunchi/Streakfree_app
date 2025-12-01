23dee97d22ca0a74a672d64efce6b6d54c308ff7
PlaysJr <jorgegael25@hotmail.com>
2025-11-30 19:29:01 -0700

    Tamanios del Login en APK corregidos y modificacion de TOASTS de Facebook y Google

--- app/src/main/java/com/example/streakfreeapp/ui/activities/Login.kt
+++ app/src/main/java/com/example/streakfreeapp/ui/activities/Login.kt
@@ -121,7 +121,7 @@ class LoginActivity : AppCompatActivity() {
             showRegisterTab()
         }
 
-        // Login
+        //Login
         buttonLogin.setOnClickListener {
             val usernameOrEmail = editTextEmail.text.toString().trim()
             val password = editTextPassword.text.toString().trim()
@@ -139,7 +139,7 @@ class LoginActivity : AppCompatActivity() {
             togglePasswordVisibility(editTextPassword, imageViewShowPassword, 0)
         }
 
-        // Registro
+        //Registro
         buttonRegister.setOnClickListener {
             val username = editTextName.text.toString().trim()
             val email = editTextEmailRegister.text.toString().trim()
@@ -161,11 +161,11 @@ class LoginActivity : AppCompatActivity() {
 
         // Botones sociales
         buttonLoginGoogle.setOnClickListener {
-            Toast.makeText(this, "Login con Google - Próximamente", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Login con Google - Se acabo el presupuesto...", Toast.LENGTH_SHORT).show()
         }
 
         buttonLoginFacebook.setOnClickListener {
-            Toast.makeText(this, "Login con Facebook - Próximamente", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Login con Facebook - Se acabo el presupuesto...", Toast.LENGTH_SHORT).show()
         }
     }
 
